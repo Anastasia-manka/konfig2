cfee0ada8bafe1801425163d0ff94a32457e938d
Anastasia <9852055031@mail.ru>
2024-12-13 00:52:11 +0300

    задания 2

--- 1.py
+++ 1.py
@@ -5,10 +5,14 @@ import yaml
 import datetime
 import graphviz
 
+
 def load_config(config_path):
     with open(config_path, 'r') as file:
         config = yaml.safe_load(file)
     return config
+
+
+import zoneinfo
 
 
 def get_commits(repo_path, after_date):
@@ -20,9 +24,10 @@ def get_commits(repo_path, after_date):
     )
 
     commits = []
+    timezone = zoneinfo.ZoneInfo("Europe/Moscow")  # Укажите часовой пояс
     for line in result.stdout.splitlines():
         hash_value, date = line.split(' ', 1)
-        commit_date = datetime.datetime.fromisoformat(date).replace(tzinfo=None)  # Исправлено
+        commit_date = datetime.datetime.fromisoformat(date).replace(tzinfo=timezone)  # Исправлено
         if commit_date > after_date:
             commits.append(hash_value)
     return commits
@@ -43,20 +48,24 @@ def build_graph(commits):
 def save_graph(dot, output_path):
     dot.render(output_path, format='png', cleanup=True)
 
+
+import zoneinfo
+
+
 def main():
     config = load_config('config.yaml')
-    after_date = datetime.datetime.fromisoformat(config['commit_date']).replace(tzinfo=None)
-    #after_date = datetime.datetime.fromisoformat(config['commit_date'])
-    
+    timezone = zoneinfo.ZoneInfo("Europe/Moscow")  # Укажите часовой пояс
+    after_date = datetime.datetime.fromisoformat(config['commit_date']).replace(tzinfo=timezone)
+
     commits = get_commits(config['repository_path'], after_date)
-    
+
     if not commits:
         print("No commits found after the specified date.")
         return
-    
+
     graph = build_graph(commits)
     save_graph(graph, config['output_image_path'])
-    
+
     print(f"Graph saved successfully to {config['output_image_path']}.")
 
 if __name__ == "__main__":
